9c4ea6f63d389bb7d29063935f0be314974169a6
Edvin Molla <edvinmolla@ucla.edu>
2023-12-28 01:11:34 -0800

    zsh-commit

--- s.py
+++ s.py
@@ -1,6 +1,6 @@
 import stripe
 
-stripe.api_key = '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'
+stripe.api_key = 'key here'
 
 email = "edvmolla@gmail.com"  # Replace with the email you want to filter by
 
